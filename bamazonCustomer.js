d1b726506f98c6708250a50f8e54335131f66022
Kristi Kacaj <kristik.web@gmail.com>
2020-06-24 18:55:54 -0400

    mysql connection established

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,20 @@
-// Required NPM packages
+//Required NPM packages
 require('dotenv').config()
 let mysql = require ("mysql");
+const { start } = require('repl');
 let inquirer = ("inquirer");
+
+//Create connection to the sql database
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: process.env.MYSQL_PASS,
+    database: 'bamazon_db'
+});
+
+
+
+
+
+
